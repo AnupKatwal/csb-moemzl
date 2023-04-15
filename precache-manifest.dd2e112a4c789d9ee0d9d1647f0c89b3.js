40d8729dc6a6f2a1adbb9aed698bfe82fc36af2a
Code Sandbox <hello@codesandbox.io>
2023-04-15 06:24:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dd2e112a4c789d9ee0d9d1647f0c89b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c33d86b6860f922c7437dd5dfa79ad6",
+    "url": "https://anupkatwal.github.io/csb-moemzl/index.html"
+  },
+  {
+    "revision": "67e8d3cb464d1ccea626",
+    "url": "https://anupkatwal.github.io/csb-moemzl/static/css/main.ff0d6d32.chunk.css"
+  },
+  {
+    "revision": "8268a0bb945486ecef3e",
+    "url": "https://anupkatwal.github.io/csb-moemzl/static/js/2.29a17a2c.chunk.js"
+  },
+  {
+    "revision": "67e8d3cb464d1ccea626",
+    "url": "https://anupkatwal.github.io/csb-moemzl/static/js/main.1d2a839c.chunk.js"
+  },
+  {
+    "revision": "6bbd87728d24504a4a2a",
+    "url": "https://anupkatwal.github.io/csb-moemzl/static/js/runtime~main.0af12eeb.js"
+  }
+]);
